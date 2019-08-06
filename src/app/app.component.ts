6592087cd9805f30a1124b2ea06f4d2cfd839d4d
chhayang <49663779+chhayang@users.noreply.github.com>
2019-08-06 09:08:51 -0400

    updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DevOpsDemo updated By Chhayang';
+  title = 'DevOps - CI/CD Sample Pipeline';
 }
